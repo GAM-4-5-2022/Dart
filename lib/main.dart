0fc723ab22ca5b092e6f8f53148fe7167afe21f6
Duš <62700665+G-rox-y@users.noreply.github.com>
2022-04-10 18:37:24 +0200

    Minor fixes
    Pojednostavnio kod,
    Malo popravio izgled UI

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,53 @@ void main() async {
   runApp(MyApp());
 }
 
+String findWords(response, green, yellow, grey, wordNum) {
+  var words = response.split('\n');
+  var bestWords = List.empty(growable: true);
+
+  int counter = 0;
+  for(int wordLoc = 0; wordLoc < words.length && counter < wordNum; wordLoc++){
+    String word = words[wordLoc];
+    bool compCheck = true;
+
+    for(int i = 0; i < green.length; i++){
+      int val = green[i][1].codeUnitAt(0)-"1".codeUnitAt(0);
+      if (word[val] != green[i][0]) compCheck = false;
+    }
+    for(int i = 0; i < yellow.length; i++){
+      int val = yellow[i][1].codeUnitAt(0)-"1".codeUnitAt(0);
+      if(word[val] == yellow[i][0]) compCheck = false;
+    }
+    for(int i = 0; i < grey.length; i++){
+      for(int j = 0; j < 5; j++){
+        if (word[j] == grey[i]) compCheck = false;
+      }
+    }
+    
+    if (!compCheck) continue;
+    bestWords.add(word.substring(0,5));
+    counter++;
+  }
+
+  if (bestWords.isEmpty){
+    return "Nije Ponađena nijedna riječ\nProvijerite jeste li ispravno upisali sve podatke";
+  }
+
+  String output = "Najbolje pronađene riječi:\n";
+  for(int i = 0; i < bestWords.length; i++){
+    output += bestWords[i];
+    if (i != bestWords.length-1) output += ", ";
+  } return output;
+}
+
+String prepareFormText(tekst) {
+  String noSpaces = "";
+  for(int i = 0; i < tekst.length; i++){
+    if (tekst[i] != " ") noSpaces += tekst[i];
+  }
+  return noSpaces.toLowerCase();
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -98,58 +145,6 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             const MyStatefulWidget(),
-            InkWell(
-              onTap: () {
-                _launchURL();
-              },
-              child: Container(
-                height: MediaQuery.of(context).size.height * 1 / 14,
-                width: MediaQuery.of(context).size.width * 3 / 7,
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Colors.red,
-                        Colors.pink,
-                        Colors.purple,
-                        Colors.deepPurple,
-                        Colors.deepPurple,
-                        Colors.indigo,
-                        Colors.green,
-                        Colors.lightGreen,
-                        Colors.lime,
-                        Colors.yellow,
-                        Colors.amber,
-                        Colors.orange,
-                        Colors.deepOrange,
-                      ]),
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const Text(""),
-                    const Text(
-                      "Otvori igru",
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white),
-                    ),
-                    Container(
-                        padding: const EdgeInsets.all(8.0),
-                        decoration: const BoxDecoration(
-                            shape: BoxShape.circle, color: Colors.white),
-                        child: const Icon(
-                          Icons.arrow_right_alt_outlined,
-                          size: 25.0,
-                          color: Colors.green,
-                        ))
-                  ],
-                ),
-              ),
-            ),
           ],
         ),
       ),
@@ -180,33 +175,23 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       child: Form(
         key: _formKey,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             TextFormField(
               controller: greenController,
               decoration: const InputDecoration(
                   hintText: 'Zelena slova',
                   hintStyle: TextStyle(
-                      color: Colors.green, fontWeight: FontWeight.bold)),
-/*             validator: (String? value1) {
-                if (value1 == null || value1.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              }, */
+                      color: Colors.green, fontWeight: FontWeight.bold)
+              ),
             ),
             TextFormField(
               controller: yellowController,
               decoration: const InputDecoration(
                   hintText: 'Žuta slova',
                   hintStyle: TextStyle(
-                      color: Colors.amber, fontWeight: FontWeight.bold)),
-/*             validator: (String? value2) {
-                if (value2 == null || value2.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              }, */
+                      color: Colors.amber, fontWeight: FontWeight.bold)
+              ),
             ),
             TextFormField(
               controller: greyController,
@@ -214,161 +199,150 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                   hintText: 'Siva slova',
                   hintStyle: TextStyle(
                       color: Color.fromARGB(255, 127, 128, 127),
-                      fontWeight: FontWeight.bold)),
-/*             validator: (String? value3) {
-                if (value3 == null || value3.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              }, */
+                      fontWeight: FontWeight.bold)
+              ),
             ),
             TextFormField(
               controller: numController,
               decoration: const InputDecoration(
-                  hintText: 'Broj ponuđenih riječi (4 je default)',
+                  hintText: 'Broj ponuđenih riječi (6 je default)',
                   hintStyle: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold)),
-/*             validator: (String? value1) {
-                if (value1 == null || value1.isEmpty) {
-                  return 'Samo brojke dopuštene';
-                }
-                return null;
-              }, */
-            ),
-            InkWell(
-              onTap: () async {
-                String response;
-
-                response = await rootBundle.loadString('assets/words.txt');
-
-                List<String> grey =
-                    greyController.text.toLowerCase().split(',');
-                List<String> yellow =
-                    yellowController.text.toLowerCase().split(',');
-                List<String> green =
-                    greenController.text.toLowerCase().split(',');
-                int wordNum = 4;
-                if (numController.text != "") {
-                  wordNum = int.parse(numController.text);
-                }
-
-                var greenDict = {};
-                var yellowDict = {};
-                String i;
-
-                if (greenController.text != "") {
-                  for (i in green) {
-                    greenDict[int.parse(i.substring(i.length - 1))] =
-                        i.substring(0, i.length - 1);
-                  }
-                }
-                String word;
-                var possibleWords = {};
-
-                var words = response
-                    .split('\n')
-                    .getRange(0, response.split('\n').length - 1);
-
-                for (word in words) {
-                  int freq = int.parse(
-                      word.substring(word.indexOf(';') + 1, word.length));
-                  word = word.substring(0, word.indexOf(';'));
-                  bool poss = true;
-                  if (greyController.text != '') {
-                    for (i in grey) {
-                      if (word.contains(i)) {
-                        poss = false;
-                      }
-                    }
-                  }
-                  if (yellowController.text != '') {
-                    for (i in yellow) {
-                      int place = int.parse(i.substring(i.length - 1));
-                      String letter = i.substring(0, i.length - 1);
-                      if (word.contains(letter) != true ||
-                          word.substring(place - 1, place) == letter) {
-                        poss = false;
-                      }
-                    }
-                  }
-                  if (greenController.text != '') {
-                    for (int j in greenDict.keys) {
-                      if (word.substring(j - 1, j) != greenDict[j]) {
-                        poss = false;
-                      }
-                    }
-                  }
-                  if (poss) {
-                    possibleWords[word] = freq;
-                  }
-                }
-
-                String output = '';
-                if (possibleWords.isNotEmpty) {
-                  for (int i = 0; i < wordNum; i++) {
-                    if (i < possibleWords.length) {
-                      if (i < wordNum - 1 && i < possibleWords.length - 1) {
-                        output += (possibleWords.keys.elementAt(i) + ', ');
-                      } else {
-                        output += possibleWords.keys.elementAt(i);
-                      }
-                    }
-                  }
-                }
-                if (possibleWords.isNotEmpty) {
-                  setState(() {
-                    log = 'Pronađeno ${possibleWords.length} riječi: $output.';
-                  });
-                } else {
-                  setState(() {
-                    log =
-                        "Nije pronađena nijedna riječ. \nProvjerite jesu li svi podaci ispravno upisani.";
-                  });
-                }
-
-                if (_formKey.currentState!.validate()) {}
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 1 / 15,
-                  width: MediaQuery.of(context).size.width * 3 / 7,
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Colors.lightBlue,
-                          Colors.cyan,
-                        ]),
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      const Text(""),
-                      const Text(
-                        "Pretraži",
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white),
-                      ),
-                      Container(
-                          padding: const EdgeInsets.all(8.0),
-                          decoration: const BoxDecoration(
-                              shape: BoxShape.circle, color: Colors.white),
-                          child: const Icon(
-                            Icons.search,
-                            size: 25.0,
-                            color: Colors.green,
-                          ))
-                    ],
-                  ),
-                ),
+                      color: Colors.black, fontWeight: FontWeight.bold)
               ),
             ),
-            Text(log),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: () async {
+                    var grey = List.empty(growable: true);
+                    var yellow = List.empty(growable: true);
+                    var green = List.empty(growable: true);
+                    int wordNum = 6;
+
+                    if (greyController.text != "") grey = prepareFormText(greyController.text).split(',');
+                    if (yellowController.text != "") yellow = prepareFormText(yellowController.text).split(',');
+                    if (greenController.text != "") green = prepareFormText(greenController.text).split(',');
+                    if (numController.text != "") wordNum = int.parse(numController.text);
+
+                    String response;
+                    response = await rootBundle.loadString('assets/words.txt');
+                    String output = findWords(response, green, yellow, grey, wordNum);
+                    setState(() {log = output;});
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 1 / 14,
+                      width: MediaQuery.of(context).size.width * 3 / 7,
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Colors.lightBlue,
+                              Colors.cyan,
+                            ]
+                        ),
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          const Text(""),
+                          const Text(
+                            "Pretraži",
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white
+                            ),
+                          ),
+                          Container(
+                              padding: const EdgeInsets.all(8.0),
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.white),
+                              child: const Icon(
+                                Icons.search,
+                                size: 25.0,
+                                color: Colors.green,
+                              )
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Text("   "),
+                InkWell(
+                  onTap: () {
+                    _launchURL();
+                  },
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 1 / 14,
+                    width: MediaQuery.of(context).size.width * 3 / 7,
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Colors.red,
+                            Colors.pink,
+                            Colors.purple,
+                            Colors.deepPurple,
+                            Colors.deepPurple,
+                            Colors.indigo,
+                            Colors.green,
+                            Colors.lightGreen,
+                            Colors.lime,
+                            Colors.yellow,
+                            Colors.amber,
+                            Colors.orange,
+                            Colors.deepOrange,
+                          ]
+                      ),
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const Text(""),
+                        const Text(
+                          "Otvori igru",
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white
+                          ),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white),
+                            child: const Icon(
+                              Icons.arrow_right_alt_outlined,
+                              size: 25.0,
+                              color: Colors.green,
+                            )
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ]
+            ),
+            Text(""),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
+                text: log,
+              ),
+            ),
           ],
         ),
       ),
